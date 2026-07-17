1cf4c63ef34cabfdce566c473968962b1f8ef5d0
Harshal Sonawane <118678847+HarshalSonawane30@users.noreply.github.com>
2026-07-17 19:39:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (83.99%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -6,11 +6,10 @@ public:
         for (; x != 0; x /= 10) {
             int digit = x % 10;
 
-            // Check for overflow
+          
             if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7))
                 return 0;
 
-            // Check for underflow
             if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8))
                 return 0;
 
