fc29fe6910ae7ea624c00d83276870609e9459f4
Harshal <harshalsonawane.comp@siem.org.in>
2026-09-16 21:03:07 +0530

    Add largest number array program

---
+++ LargNumArr.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class LargNumArr {
+
+    public static int findMax(int[] nums) {
+
+        int max = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = findMax(nums);
+
+        System.out.println(result);
+
+        
+    }
+}
